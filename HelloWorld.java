76420bd57219ad907b7ab0377254ee5011340606
Sonu kumar <sonukumarshk47@gmail.com>
2026-02-23 13:58:02 +0530

    Added Java project structure

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class Helloworld
-{
-public static void main()
-{
-System.out.println("My name is Sonuuuuu");
-}
-}
---
+++ main.java
@@ -0,0 +1,7 @@
+package com.oopsbannerapp;
+
+public class Main { 
+    public static void main(String[] args) {
+        System.out.println("Welcome to OOPS Banner App");
+    }
+}
