d0696f8842a67d81d9cf7964c3277a072aa36841
Sonu kumar <sonukumarshk47@gmail.com>
2026-02-23 14:04:12 +0530

    Fixing class name and package structure

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
-package com.oopsbannerapp;
+package com.oopsbannerapp; // Matches your folder path
 
 public class Main { 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // String[] args is required
         System.out.println("Welcome to OOPS Banner App");
     }
 }
