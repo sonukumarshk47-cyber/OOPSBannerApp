7b6f2eac9a8903ca532e3713b6f6381682faf422
Sonu kumar <sonukumarshk47@gmail.com>
2026-02-23 14:16:26 +0530

    UC2 - Banner Print Statements

---
+++ com/oopsbannerapp/Main.java
@@ -0,0 +1,7 @@
+package com.oopsbannerapp; // Matches your folder path
+
+public class Main { 
+    public static void main(String[] args) { // String[] args is required
+        System.out.println(" OOPS Banner ");
+    }
+}
